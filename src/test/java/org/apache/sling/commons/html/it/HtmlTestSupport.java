3447042385ca005a1a2df987de9f4f24bb6a467c
Oliver Lietz <olli@apache.org>
2019-06-15 16:43:53 +0200

    SLING-8508 Make testing compliant with Java 9 and higher
    * Use Sling Bundle Parent 35
    * Update Pax Exam to 4.13.1
    * Update Testing PaxExam to 3.0.0
    * Update Felix Framework to 6.0.3
    * Use logback Option

--- src/test/java/org/apache/sling/commons/html/it/HtmlTestSupport.java
+++ src/test/java/org/apache/sling/commons/html/it/HtmlTestSupport.java
@@ -19,19 +19,18 @@
 package org.apache.sling.commons.html.it;
 
 import org.apache.sling.testing.paxexam.TestSupport;
-import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.options.ModifiableCompositeOption;
 
+import static org.apache.sling.testing.paxexam.SlingOptions.logback;
 import static org.apache.sling.testing.paxexam.SlingOptions.scr;
 import static org.apache.sling.testing.paxexam.SlingVersionResolver.SLING_GROUP_ID;
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-import org.ops4j.pax.exam.util.PathUtils;
 
 public abstract class HtmlTestSupport extends TestSupport {
 
-    public Option baseConfiguration() {
+    public ModifiableCompositeOption baseConfiguration() {
         return composite(
             super.baseConfiguration(),
             // Sling Commons HTML
@@ -42,18 +41,8 @@ public abstract class HtmlTestSupport extends TestSupport {
             mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
             mavenBundle().groupId("org.apache.commons").artifactId("commons-lang3").versionAsInProject(),
             junitBundles(),
-            logging()
+            logback()
         );
     }
 
-    protected Option logging() {
-        final String filename = String.format("file:%s/src/test/resources/logback.xml", PathUtils.getBaseDir());
-        return composite(
-            systemProperty("logback.configurationFile").value(filename),
-            mavenBundle().groupId("org.slf4j").artifactId("slf4j-api").version("1.7.21"),
-            mavenBundle().groupId("org.slf4j").artifactId("jcl-over-slf4j").version("1.7.21"),
-            mavenBundle().groupId("ch.qos.logback").artifactId("logback-core").version("1.1.7"),
-            mavenBundle().groupId("ch.qos.logback").artifactId("logback-classic").version("1.1.7")
-        );
-    }
 }
